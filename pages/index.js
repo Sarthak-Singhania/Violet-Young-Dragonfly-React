306247df5788a46583b95a349b0506a752aa8a61
Sarthak Singhania <40860138+Sarthak-Singhania@users.noreply.github.com>
2022-12-10 10:16:11 +0530

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,420 @@
+import React from 'react'
+import Image from 'next/image'
+import Head from 'next/head'
+
+import NavigationLinks from '../components/navigation-links'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Assemblage</title>
+          <meta property="og:title" content="Assemblage" />
+        </Head>
+        <span className="top-bar">
+          <span>
+            For customizations or personal assistance, WhatsApp us at
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <a
+            href="https://api.whatsapp.com/send?phone=918920651205&amp;text=Hi!%20Could%20you%20help%20me%20with%20a%20few%20queries!"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="home-link"
+          >
+            +91 8920651205
+          </a>
+        </span>
+        <header data-role="Header" className="home-header">
+          <Image
+            alt="logo"
+            src="/playground_assets/transparent%20logo-200h.png"
+            className="home-image"
+            width={209}
+            height={85}
+          />
+          <div className="home-nav">
+            <div
+              data-thq="thq-dropdown"
+              className="home-thq-dropdown list-item"
+            >
+              <div
+                data-thq="thq-dropdown-toggle"
+                className="home-dropdown-toggle"
+              >
+                <span className="home-text2">Serveware</span>
+              </div>
+              <ul data-thq="thq-dropdown-list" className="home-dropdown-list">
+                <li data-thq="thq-dropdown" className="home-dropdown list-item">
+                  <div
+                    data-thq="thq-dropdown-toggle"
+                    className="home-dropdown-toggle1"
+                  >
+                    <span className="home-text3">Sub-menu Item</span>
+                  </div>
+                </li>
+                <li
+                  data-thq="thq-dropdown"
+                  className="home-dropdown1 list-item"
+                >
+                  <div
+                    data-thq="thq-dropdown-toggle"
+                    className="home-dropdown-toggle2"
+                  >
+                    <span className="home-text4">Sub-menu Item</span>
+                  </div>
+                </li>
+                <li
+                  data-thq="thq-dropdown"
+                  className="home-dropdown2 list-item"
+                >
+                  <div
+                    data-thq="thq-dropdown-toggle"
+                    className="home-dropdown-toggle3"
+                  >
+                    <span className="home-text5">Sub-menu Item</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="home-btn-group">
+            <svg viewBox="0 0 1024 1024" className="home-search">
+              <path d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"></path>
+            </svg>
+            <svg viewBox="0 0 1024 1024" className="home-wishlist">
+              <path d="M950.857 340.571c0-160.571-108.571-194.286-200-194.286-85.143 0-181.143 92-210.857 127.429-13.714 16.571-42.286 16.571-56 0-29.714-35.429-125.714-127.429-210.857-127.429-91.429 0-200 33.714-200 194.286 0 104.571 105.714 201.714 106.857 202.857l332 320 331.429-319.429c1.714-1.714 107.429-98.857 107.429-203.429zM1024 340.571c0 137.143-125.714 252-130.857 257.143l-356 342.857c-6.857 6.857-16 10.286-25.143 10.286s-18.286-3.429-25.143-10.286l-356.571-344c-4.571-4-130.286-118.857-130.286-256 0-167.429 102.286-267.429 273.143-267.429 100 0 193.714 78.857 238.857 123.429 45.143-44.571 138.857-123.429 238.857-123.429 170.857 0 273.143 100 273.143 267.429z"></path>
+            </svg>
+            <svg viewBox="0 0 1024 1024" className="home-account">
+              <path d="M512 598q108 0 225 47t117 123v86h-684v-86q0-76 117-123t225-47zM512 512q-70 0-120-50t-50-120 50-121 120-51 120 51 50 121-50 120-120 50z"></path>
+            </svg>
+            <svg viewBox="0 0 1024 1024" className="home-cart">
+              <path d="M1004 804.571l20 178.857c1.143 10.286-2.286 20.571-9.143 28.571-6.857 7.429-17.143 12-27.429 12h-950.857c-10.286 0-20.571-4.571-27.429-12-6.857-8-10.286-18.286-9.143-28.571l20-178.857h984zM950.857 325.143l49.143 442.857h-976l49.143-442.857c2.286-18.286 17.714-32.571 36.571-32.571h146.286v73.143c0 40.571 32.571 73.143 73.143 73.143s73.143-32.571 73.143-73.143v-73.143h219.429v73.143c0 40.571 32.571 73.143 73.143 73.143s73.143-32.571 73.143-73.143v-73.143h146.286c18.857 0 34.286 14.286 36.571 32.571zM731.429 219.429v146.286c0 20-16.571 36.571-36.571 36.571s-36.571-16.571-36.571-36.571v-146.286c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286v146.286c0 20-16.571 36.571-36.571 36.571s-36.571-16.571-36.571-36.571v-146.286c0-121.143 98.286-219.429 219.429-219.429s219.429 98.286 219.429 219.429z"></path>
+            </svg>
+          </div>
+          <div data-role="BurgerMenu" className="home-burger-menu">
+            <svg viewBox="0 0 1024 1024" className="home-icon04">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div data-role="MobileMenu" className="home-mobile-menu">
+            <div className="home-nav1">
+              <div className="home-container1">
+                <img
+                  alt="image"
+                  src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+                  className="home-image1"
+                />
+                <div data-role="CloseMobileMenu" className="home-menu-close">
+                  <svg viewBox="0 0 1024 1024" className="home-icon06">
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <NavigationLinks rootClassName="rootClassName11"></NavigationLinks>
+            </div>
+            <div>
+              <svg viewBox="0 0 950.8571428571428 1024" className="home-icon08">
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon10">
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg viewBox="0 0 602.2582857142856 1024" className="home-icon12">
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+            justify-content: flex-start;
+          }
+          .home-link {
+            color: #e83647;
+          }
+          .home-header {
+            width: 100%;
+            display: flex;
+            max-width: var(--dl-size-size-maxwidth);
+            box-shadow: 5px 5px 10px 0px #d4d4d4;
+            align-items: center;
+            padding-top: var(--dl-space-space-twounits);
+            padding-left: var(--dl-space-space-threeunits);
+            padding-right: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-twounits);
+            justify-content: space-between;
+          }
+          .home-image {
+            width: 209px;
+            height: 85px;
+          }
+          .home-nav {
+            display: flex;
+          }
+          .home-thq-dropdown {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            border-radius: var(--dl-radius-radius-radius2);
+          }
+          .home-dropdown-toggle {
+            fill: #595959;
+            color: #595959;
+            width: 100%;
+            display: inline-flex;
+            align-items: center;
+            padding-top: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-unit);
+            border-radius: var(--dl-radius-radius-radius2);
+            padding-right: var(--dl-space-space-unit);
+            padding-bottom: var(--dl-space-space-halfunit);
+          }
+          .home-text2 {
+            font-size: 14px;
+            font-style: normal;
+            text-align: center;
+            font-weight: 500;
+            margin-right: 4px;
+            vertical-align: middle;
+          }
+          .home-dropdown-list {
+            left: 0%;
+            width: max-content;
+            display: none;
+            z-index: 100;
+            position: absolute;
+            min-width: 100%;
+            transition: 0.3s;
+            align-items: stretch;
+            border-color: #d9d9d9;
+            border-width: 1px;
+            border-radius: var(--dl-radius-radius-radius4);
+            flex-direction: column;
+            list-style-type: none;
+            background-color: var(--dl-color-gray-white);
+            list-style-position: inside;
+          }
+          .home-dropdown {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            border-radius: var(--dl-radius-radius-radius2);
+          }
+          .home-dropdown-toggle1 {
+            fill: #595959;
+            color: #595959;
+            width: 100%;
+            display: inline-flex;
+            transition: 0.3s;
+            align-items: center;
+            padding-top: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-unit);
+            border-radius: var(--dl-radius-radius-radius4);
+            padding-right: var(--dl-space-space-unit);
+            padding-bottom: var(--dl-space-space-halfunit);
+          }
+          .home-dropdown-toggle1:hover {
+            fill: #fff;
+            color: #fff;
+            background-color: #595959;
+          }
+          .home-text3 {
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 500;
+          }
+          .home-dropdown1 {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            border-radius: var(--dl-radius-radius-radius2);
+          }
+          .home-dropdown-toggle2 {
+            fill: #595959;
+            color: #595959;
+            width: 100%;
+            display: inline-flex;
+            transition: 0.3s;
+            align-items: center;
+            padding-top: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-unit);
+            border-radius: var(--dl-radius-radius-radius4);
+            padding-right: var(--dl-space-space-unit);
+            padding-bottom: var(--dl-space-space-halfunit);
+          }
+          .home-dropdown-toggle2:hover {
+            fill: #fff;
+            color: #fff;
+            background-color: #595959;
+          }
+          .home-text4 {
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 500;
+          }
+          .home-dropdown2 {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            border-radius: var(--dl-radius-radius-radius2);
+          }
+          .home-dropdown-toggle3 {
+            fill: #595959;
+            color: #595959;
+            width: 100%;
+            display: inline-flex;
+            transition: 0.3s;
+            align-items: center;
+            padding-top: var(--dl-space-space-halfunit);
+            padding-left: var(--dl-space-space-unit);
+            border-radius: var(--dl-radius-radius-radius4);
+            padding-right: var(--dl-space-space-unit);
+            padding-bottom: var(--dl-space-space-halfunit);
+          }
+          .home-dropdown-toggle3:hover {
+            fill: #fff;
+            color: #fff;
+            background-color: #595959;
+          }
+          .home-text5 {
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 500;
+          }
+          .home-btn-group {
+            gap: var(--dl-space-space-unit);
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            justify-content: center;
+          }
+          .home-search {
+            width: 24px;
+            height: 24px;
+          }
+          .home-wishlist {
+            width: 24px;
+            height: 24px;
+          }
+          .home-account {
+            width: 24px;
+            height: 24px;
+          }
+          .home-cart {
+            width: 24px;
+            height: 24px;
+          }
+          .home-burger-menu {
+            display: none;
+          }
+          .home-icon04 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .home-mobile-menu {
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100vh;
+            display: none;
+            padding: 32px;
+            z-index: 100;
+            position: absolute;
+            flex-direction: column;
+            justify-content: space-between;
+            background-color: #fff;
+          }
+          .home-nav1 {
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .home-container1 {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            margin-bottom: var(--dl-space-space-threeunits);
+            justify-content: space-between;
+          }
+          .home-image1 {
+            height: 2rem;
+          }
+          .home-menu-close {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .home-icon06 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .home-icon08 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+            margin-right: var(--dl-space-space-twounits);
+          }
+          .home-icon10 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+            margin-right: var(--dl-space-space-twounits);
+          }
+          .home-icon12 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          @media (max-width: 767px) {
+            .home-header {
+              padding-left: var(--dl-space-space-twounits);
+              padding-right: var(--dl-space-space-twounits);
+            }
+            .home-nav {
+              display: none;
+            }
+            .home-btn-group {
+              display: none;
+            }
+            .home-burger-menu {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+          }
+          @media (max-width: 479px) {
+            .home-header {
+              padding: var(--dl-space-space-unit);
+              padding-right: 16px;
+            }
+            .home-mobile-menu {
+              padding: 16px;
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
